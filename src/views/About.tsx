b951004d02b86224caa7b54b22515fe54f5a7a5d
16Yongjin <yongjin0802@gmail.com>
2021-06-25 18:30:46 +0900

    About 라우터에 노션 소개 페이지 임베딩

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -3,22 +3,26 @@ import styled from 'styled-components'
 
 const Section = styled.section`
   position: relative;
+  padding: 0;
+  height: calc(100vh - 60px - 2rem);
 
   .title {
     font-family: 'Godo';
     font-size: 2rem;
     font-weight: bold;
   }
+
+  .about {
+    width: 100%;
+    height: 100%;
+    border: none;
+  }
 `
 
 export const About = () => {
   return (
     <Section className="section">
-      <div className="container">
-        <header className="title">
-          <h2 className="title">About</h2>
-        </header>
-      </div>
+      <iframe className="about" src="embed/about-embed.html" title="about" />
     </Section>
   )
 }
